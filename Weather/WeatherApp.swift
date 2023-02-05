182cb29821a913500dfd05d49c959fe20e055480
Taha Tuna <tahatuna777@gmail.com>
2023-02-05 19:55:52 +0100

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Taha Tuna on 5.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
